4d0879a0db431f897159b680e011bb3932c8f248
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-23 11:24:50 +0200

    Update controller for fetching

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,10 +1,10 @@
-class GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   def index
-    @greetings = Greeting.all
-    if @greetings.empty?
-       json_response({msg: 'No Greetings at this time'}, 400)
-    else
-        render json: @greetings[rand(0...@greetings.length - 1)]
-    end
+      @greetings = Greeting.all
+      if @greetings.empty?
+         json_response({msg: 'No Greetings at this time'}, 400)
+      else
+          render json: @greetings[rand(0...@greetings.length - 1)]
+      end
   end
 end
--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,2 +1,2 @@
-class Message < ApplicationRecord
+class Greeting < ApplicationRecord
 end
